a51117f1c356d1663d6f09d778370afaa9f0a449
StonyMoon <zjmeow@gmail.com>
2018-12-18 13:06:34 +0800

    配置 swagger 和 ModelMapper

---
+++ src/main/java/com/zjmeow/bubble/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.zjmeow.bubble.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: swagger2 文档配置
+ **/
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
